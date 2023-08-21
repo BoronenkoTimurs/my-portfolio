6f408d32a54ddb8ebdf46947bdcef2d9adf81561
timurcsdd <timurubica@bk.ru>
2023-08-21 16:40:48 +0300

    Build work component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Work from "./components/Work";
 function App() {
   return (
     <div>
       <NavBar />
       <Home />
-      <About/>
-      <Skills/>
+      <About />
+      <Skills />
+      <Work />
     </div>
   );
 }
