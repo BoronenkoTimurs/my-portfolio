98bd875999d91d64ed17bdb31e2b3d17455dbf9e
BoronenkoTimurs <64987379+BoronenkoTimurs@users.noreply.github.com>
2024-10-26 23:09:22 +0300

    Add projects.

--- src/configs/const.js
+++ src/configs/const.js
@@ -1,6 +1,9 @@
 import GameHub from '../assets/projects/gamehub.JPG';
 import Mosh from '../assets/projects/mosh.JPG';
 import AutomationCamp from '../assets/projects/automationcamp.png';
+import MonopolyRPG from '../assets/projects/monopoly-RPG.png';
+import EcommerceDocs from '../assets/projects/ecommerce-shop/ecommerce-docs.png';
+import NotFound from '../assets/projects/404.webp';
 
 export const PROJECTS = [
   {
@@ -23,5 +26,33 @@ export const PROJECTS = [
     demoLink: "https://play1.automationcamp.ir/",
     codeLink: "https://github.com/BoronenkoTimurs/automatization-PW",
     backgroundImage: AutomationCamp,
+  },
+  {
+    id: 4,
+    title: "Vanila JS Game - Monopoly RPG",
+    demoLink: "https://sgames.x10.mx/",
+    codeLink: "https://github.com/BoronenkoTimurs/monopoly-RPG-client",
+    backgroundImage: MonopolyRPG,
+  },
+  {
+    id: 5,
+    title: "Ecommerce-shop project admin - NextJS",
+    demoLink: "#",
+    codeLink: "https://github.com/BoronenkoTimurs/ecommerce-admin",
+    backgroundImage: NotFound,
+  },
+  {
+    id: 6,
+    title: "Ecommerce-shop project client - NextJS",
+    demoLink: "#",
+    codeLink: "https://github.com/BoronenkoTimurs/ecommerce-client",
+    backgroundImage: NotFound,
+  },
+  {
+    id: 7,
+    title: "Ecommerce-shop project documentation",
+    demoLink: "https://main-documentation.netlify.app/docs/intro",
+    codeLink: "https://github.com/BoronenkoTimurs/ecommerce-documentation",
+    backgroundImage: EcommerceDocs,
   },
 ];
